ff6dc625ffae955665b30436d5dd0a98b3ea149d
qweri0p <71005650+qweri0p@users.noreply.github.com>
2021-12-13 08:56:15 +0100

    baded

--- commands/hello.js
+++ commands/hello.js
@@ -2,7 +2,7 @@ function command(message) {
   message.channel.send("Hello there!");
 }
 function help(message, Discord) {
-    const helpEmbed = new Discord.MessageEmbed().setTitle("Plant gang moment").setDescription("Plant gang can do:").setColor("#00ff00").setURL('https://qweri0p.github.io/plantbot').addField("p plant", "Do plant thing", true).addField("p plm", "Plant Lives Matter", true).addField("p based", "true", true).addField("p sus", ":exploding_head:", true).setFooter("Plant gang")
+    const helpEmbed = new Discord.MessageEmbed().setTitle("Plant gang moment").setDescription("Plant gang can do:").setColor("#00ff00").setURL('https://qweri0p.github.io/plantbot').addField("p plant", "Do plant thing", true).addField("p plm", "Plant Lives Matter", true).addField("p based", "true", true).addField("p sus", "amognus sus", true).setFooter("Plant gang")
     
     message.channel.send({ embeds:[helpEmbed] });
 }
